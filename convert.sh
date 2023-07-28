049359cb254ef6ea85536a237493f0d27dde2951
s32x <steven@swolfe.me>
2023-07-28 18:32:51 -0500

    Adding base64 encoding

--- convert.sh
+++ convert.sh
@@ -1,2 +1,4 @@
 docker run -i --rm quay.io/coreos/butane:release --strict < portainer.bu > portainer.ign
+cat portainer.ign | base64 > portainer.ign.base64
 docker run -i --rm quay.io/coreos/butane:release --strict < rancher.bu > rancher.ign
+cat rancher.ign | base64 > rancher.ign.base64
